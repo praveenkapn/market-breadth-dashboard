02d98c82122a5e3a5fdf8d5f0a25f5780432ce9c
Praveen Kumar S <120298358+praveenkapn@users.noreply.github.com>
2026-06-13 21:22:16 +0530

    Update data_fetcher.py

--- market_breadth_dashboard/data_fetcher.py
+++ market_breadth_dashboard/data_fetcher.py
@@ -1,46 +1,28 @@
 import pandas as pd
-import requests
-from io import StringIO
-
 import streamlit as st
 
-INDEX_URLS = {
-    "Nifty 50": "https://www.niftyindices.com/IndexConstituent/ind_nifty50list.csv",
-    "Nifty 500": "https://www.niftyindices.com/IndexConstituent/ind_nifty500list.csv",
-    "Nifty Midcap 150": "https://www.niftyindices.com/IndexConstituent/ind_niftymidcap150list.csv",
-    "Nifty Smallcap 250": "https://www.niftyindices.com/IndexConstituent/ind_niftysmallcap250list.csv"
+LOCAL_FILES = {
+    "Nifty 50": "data/nifty50.csv",
+    "Nifty 500": "data/nifty500.csv",
+    "Nifty Midcap 150": "data/midcap150.csv",
+    "Nifty Smallcap 250": "data/smallcap250.csv"
 }
+
+
 @st.cache_data(ttl=86400)
 def get_index_constituents(index_name):
 
-    url = INDEX_URLS[index_name]
-
-    headers = {
-        "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/137.0.0.0 Safari/537.36"
-        ),
-        "Accept": "text/csv,text/plain,*/*",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Referer": "https://www.niftyindices.com/",
-        "Origin": "https://www.niftyindices.com"
-    }
-
-    session = requests.Session()
-
-    response = session.get(
-        url,
-        headers=headers,
-        timeout=60
-    )
-
-    response.raise_for_status()
-
     df = pd.read_csv(
-        StringIO(response.text)
+        LOCAL_FILES[index_name]
     )
 
-    symbols = df["Symbol"].dropna().tolist()
+    symbols = (
+        df["Symbol"]
+        .dropna()
+        .tolist()
+    )
 
-    return [f"{symbol}.NS" for symbol in symbols]
+    return [
+        f"{symbol}.NS"
+        for symbol in symbols
+    ]
